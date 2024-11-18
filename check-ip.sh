d952018a55bdfe6a049dbab1086b57fe15ef68bb
Irimia David <david.irimia1@s.unibuc.ro>
2024-11-18 17:28:08 +0200

    add check-ip.sh

---
+++ check-ip.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cat /etc/hosts | while IFS= read -r line; do
+    if [[ "$ip" == \#* ]]; then
+	break
+
+    ip_file=$(echo "$line" | cut -d ' ' -f 1)
+    name_file=$(echo "$line" | cut -d ' ' -f 2)
+
+   
+    if [[ "$name_file" == "localhost" || "$name_file" == "DESKTOP-PS905TO" ]]; then
+        ip_lookup=$(nslookup "$name_file" | grep -m 1 'Address:' | tail -1 | cut -d ' ' -f 2)
+    else
+        ip_lookup=$(nslookup "$name_file" 8.8.8.8 | grep -m 1 'Address:' | tail -1 | cut -d ' ' -f 2)
+    fi
+
+ 
+    if [[ "$ip_nslookup" != "$ip_file" ]]; then
+        echo "Bogus IP for $name_file in /etc/hosts!"
+    fi
+done
